46e394d569408b14ccc9f96465b6b6ca535e0066
sloniewski <sloniewski.maciej@gmail.com>
2017-11-12 15:52:30 +0100

    refactored generics

--- movies_database/movies/views.py
+++ movies_database/movies/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 from django.shortcuts import get_object_or_404
 
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.views import APIView, Response
 from rest_framework import status
 
@@ -10,38 +10,9 @@ from movies.models import Movie
 from movies.serializers import MovieSerializer
 
 
-class MovieApiView(APIView):
-
-    def get_object(self, pk):
-        return get_object_or_404(Movie, pk=pk)
-
-    def get(self, request, pk):
-        movie = self.get_object(pk)
-        serializer = MovieSerializer(movie, context={'request': request})
-        return Response(serializer.data)
-
-    def delete(self, request, pk):
-        movie = self.get_object(pk)
-        movie.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-    def put(self, request, pk):
-        movie = self.get_object(pk)
-        serializer = MovieSerializer(
-            instance=movie,
-            context={'request': request},
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(
-                serializer.data,
-                status=status.HTTP_200_OK,
-            )
-        else:
-            return Response(
-                serializer.errors,
-                status=status.HTTP_400_BAD_REQUEST
-            )
+class MovieApiView(RetrieveUpdateDestroyAPIView):
+    serializer_class = MovieSerializer
+    queryset = Movie
 
 
 class MovieListApiView(ListCreateAPIView):

f7254bb410438d8777fc2dd8eff98094417b0077
sloniewski <sloniewski.maciej@gmail.com>
2017-11-12 14:27:20 +0100

    movie put verb

--- movies_database/movies/views.py
+++ movies_database/movies/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from django.shortcuts import get_object_or_404
 
 from rest_framework.views import APIView, Response
+from rest_framework import status
 
 from movies.models import Movie
 from movies.serializers import MovieSerializer
@@ -17,6 +18,29 @@ class MovieApiView(APIView):
         movie = self.get_object(pk)
         serializer = MovieSerializer(movie, context={'request': request})
         return Response(serializer.data)
+
+    def delete(self, request, pk):
+        movie = self.get_object(pk)
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def put(self, request, pk):
+        movie = self.get_object(pk)
+        serializer = MovieSerializer(
+            instance=movie,
+            context={'request': request},
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK,
+            )
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
 
 class MovieListApiView(APIView):

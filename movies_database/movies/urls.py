06713f6b2d7c3a763e4980c426683238a54714d7
sloniewski <sloniewski.maciej@gmail.com>
2017-12-02 01:17:58 +0100

    movies views changed to generic viewsets

--- movies_database/movies/urls.py
+++ movies_database/movies/urls.py
@@ -1,16 +1,20 @@
 from django.conf.urls import url
 from movies import views
 
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register(
+    'movie',
+    views.MovieView,
+    base_name='movie',
+)
+
 
 urlpatterns = [
 
-    url(r'movie/(?P<pk>\d+)/$',
-        views.MovieApiView.as_view(), name='movie-detail'),
-
-    url(r'movies/$',
-        views.MovieListApiView.as_view(), name='movie-list'),
-
     url(r'movies/genre/(?P<pk>\d+)/$',
         views.GenreMoviesMoviesApiListView.as_view(), name='genre-movie-list'),
-
 ]
+
+urlpatterns += router.urls
--- movies_database/movies/views.py
+++ movies_database/movies/views.py
@@ -1,17 +1,26 @@
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from django.shortcuts import get_object_or_404
+
+from rest_framework import status
+from rest_framework.views import Response
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import detail_route
 
 from movies.models import Movie, Genre
 from movies.serializers import MovieDetailSerializer, MovieListSerializer
 
 
-class MovieApiView(RetrieveUpdateDestroyAPIView):
-    serializer_class = MovieDetailSerializer
-    queryset = Movie
-
-
-class MovieListApiView(ListCreateAPIView):
+class MovieView(ModelViewSet):
+    model = Movie
     serializer_class = MovieListSerializer
     queryset = Movie.objects.all()
+
+    @detail_route(methods=['get'])
+    def details(self, request, pk):
+        serializer = MovieDetailSerializer(
+            instance=get_object_or_404(self.model, pk=pk),
+        )
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
 
 
 class GenreMoviesMoviesApiListView(ListCreateAPIView):

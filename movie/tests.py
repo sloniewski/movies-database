a52025a070d60edc04d2398a5167d1ca36300674
sloniewski <sloniewski.maciej@gmail.com>
2019-01-26 23:34:16 +0100

    basic test

--- movie/tests.py
+++ movie/tests.py
@@ -1,4 +1,9 @@
+import json
+
+from django.shortcuts import reverse
 from django.test import TestCase
+
+from rest_framework.test import APIClient
 
 from movie.models import Movie
 
@@ -6,4 +11,27 @@ from movie.models import Movie
 class TestMovieViews(TestCase):
 
     def setUp(self):
-        pass
+        self.client = APIClient()
+        self.movie = Movie.objects.create(
+            title='bugs bunny',
+            description='carrots are great',
+            year=2001,
+        )
+
+    def test_get_list_movies(self):
+        url = reverse('movie:movie-list')
+        response = self.client.get(url)
+        self.assertEqual(
+            json.loads(response.content).pop().get('title'), 'bugs bunny')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response['Content-Type'], 'application/json')
+
+    def test_post_movie(self):
+        url = reverse('movie:movie-list')
+        data = {
+            'title': 'huntin with elmer',
+            'description': 'huntin rabbits',
+            'year': 2003,
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, 201)
--- users/permissions.py
+++ users/permissions.py
@@ -6,5 +6,4 @@ class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
             return True
-
         return request.user.is_staff

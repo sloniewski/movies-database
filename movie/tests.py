e4c09ddb24be6d804b9dcb82b25d9100ea9e9852
sloniewski <sloniewski.maciej@gmail.com>
2019-01-27 01:34:00 +0100

    authentication test

--- movie/tests.py
+++ movie/tests.py
@@ -1,11 +1,15 @@
 import json
 
+from django.contrib.auth import get_user_model
 from django.shortcuts import reverse
 from django.test import TestCase
-
 from rest_framework.test import APIClient
+from rest_framework.authtoken.models import Token
 
 from movie.models import Movie
+
+
+User = get_user_model()
 
 
 class TestMovieViews(TestCase):
@@ -17,6 +21,15 @@ class TestMovieViews(TestCase):
             description='carrots are great',
             year=2001,
         )
+
+        self.test_username = 'duffy'
+        self.test_password = 'duck'
+        self.user = User.objects.create_user(
+            username=self.test_username,
+            password=self.test_password,
+            is_staff=True,
+        )
+        self.token = Token.objects.create(user=self.user)
 
     def test_get_list_movies(self):
         url = reverse('movie:movie-list')
@@ -33,5 +46,6 @@ class TestMovieViews(TestCase):
             'description': 'huntin rabbits',
             'year': 2003,
         }
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, 201)
